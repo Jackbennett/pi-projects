f5bbd8d7484b1ad182be0e122ca741843d7783f5
Jack Bennett <me@jackben.net>
2012-11-01 16:09:18 +0000

    making the LEDs sweep

--- cylon.py
+++ cylon.py
@@ -6,17 +6,27 @@ from array import *
 
 piface.pfio.init()
 Leds = []
+count = True
 
 for i in range(1,9):
 	Leds.append(piface.pfio.LED(i))
 
 def toggle(light):
 	light.turn_on()
-	time.sleep(.1)
+	time.sleep(.05)
 	light.turn_off()
 
 while True:
-	for n in Leds:
-		toggle(n)
+	""" Count up the leds """
+	if count:
+		for n in Leds:
+			toggle(n)
+		count = False
+
+	"""Count Down the leds"""
+	if count == False:
+		for n in reversed(Leds):
+			toggle(n)
+		count = True
 
 # vim:ts=2:sw=2:sts=2:et:ft=python
